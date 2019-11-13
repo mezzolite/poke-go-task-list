49b90ef95d090c1a27303242ec34d8ac0a4234e0
Amy Nguyen <amy.nguyen730@gmail.com>
2019-11-13 06:38:50 -0700

    refactors team.js and app.js

--- frontend/app.js
+++ frontend/app.js
@@ -4,20 +4,24 @@ document.body.append(cardContainer)
 
 fetch("http://localhost:3000/teams")
     .then(response => response.json())
-    .then(teams => teams.map(team => {
-        const teamName = document.createElement('h2')
-        const teamImage = document.createElement('img')
-        const teamButton = document.createElement('button')
+    .then(createTeamCards)
 
-        teamName.innerText = `Team ${team.name}` 
-        teamImage.src = team.image
-        teamButton.innerHTML = `<a href=team.html?id=${team.id}>Enter Team ${team.name}</a>`
+function createTeamCards(teams){
+    teams.map(teamsCardAttributes)
+}
 
-        const teamCard = document.createElement('div')
-        teamCard.className = "teamCard"
-        
-        teamCard.append(teamName, teamImage, teamButton)
-        cardContainer.append(teamCard)
-    })
-    )
+function teamsCardAttributes(team){
+    const teamName = document.createElement('h2')
+    const teamImage = document.createElement('img')
+    const teamButton = document.createElement('button')
+
+    teamName.innerText = `Team ${team.name}` 
+    teamImage.src = team.image
+    teamButton.innerHTML = `<a href=team.html?id=${team.id}>Enter Team ${team.name}</a>`
+
+    const teamCard = document.createElement('div')
+    teamCard.className = "teamCard"
     
+    teamCard.append(teamName, teamImage, teamButton)
+    cardContainer.append(teamCard)
+}    
